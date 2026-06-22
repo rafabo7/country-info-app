16c619abe7c13ddbca15f89bdeac50bd87c2870a
Rafael Botella <122386430+rafabo7@users.noreply.github.com>
2026-06-22 09:28:12 +0200

    feat: search and display countries - app.config: provide HttpClient with fetch - Rest-country-response-interface: import interface for api response. - country-interface: add own interface for countries data

---
+++ src/app/country/interfaces/country.interface.ts
@@ -0,0 +1,19 @@
+export interface Country {
+  areaKm: number,
+  capital: Capital
+  ccn3: string,
+  flag: string,
+  icon: string,
+  name: string,
+  population: number,
+}
+
+interface Capital {
+  name: string,
+  coordinates: Coordinates
+}
+
+interface Coordinates {
+  lat: number,
+  lng: number
+}
