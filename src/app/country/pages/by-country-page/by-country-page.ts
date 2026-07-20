937cdda2afc7cb45d2c3bc6ce4921ca153a88d5f
Rafael Botella <122386430+rafabo7@users.noreply.github.com>
2026-07-20 11:38:12 +0200

    feat: preserve the search state  via url param - apply to by-country component

--- src/app/country/pages/by-country-page/by-country-page.ts
+++ src/app/country/pages/by-country-page/by-country-page.ts
@@ -4,6 +4,7 @@ import { CountrySearchInput } from "../../components/country-search-input/countr
 import { CountryList } from "../../components/country-list/country-list";
 import { firstValueFrom, of } from 'rxjs';
 import { CountryService } from '../../services/country.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'by-country-page',
@@ -13,12 +14,23 @@ import { CountryService } from '../../services/country.service';
 export class ByCountryPage {
 
   countryService = inject(CountryService)
-  query = signal('')
+
+  activatedRoute = inject(ActivatedRoute)
+
+  router = inject(Router)
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? ''
+
+  query = signal(this.queryParam)
 
   countryResource = rxResource({
     params: () => ({ query: this.query() }),
     stream: ({ params }) => {
-      if ( !params.query ) return of([])
+      if (!params.query) return of([])
+      this.router.navigate(['/country/by-country'], {
+        queryParams: {
+          query: params.query
+        }
+      })
       return this.countryService.searchByCountryName(params.query)
     }
   })
