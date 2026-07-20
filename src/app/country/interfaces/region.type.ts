4f9e713996626f3b45e9676af8482f130f565060
Rafael Botella <122386430+rafabo7@users.noreply.github.com>
2026-07-20 12:23:13 +0200

    feat: preserve selected region state on navigation - refact region.type to use it as a single soruce of true with type guard - use activatedRoute and router to apply same feature to by-region-page when selecting a region

--- src/app/country/interfaces/region.type.ts
+++ src/app/country/interfaces/region.type.ts
@@ -1,7 +1,14 @@
-export type Region =
-  | 'Africa'
-  | 'Americas'
-  | 'Asia'
-  | 'Europe'
-  | 'Oceania'
-  | 'Antarctic';
+export const REGIONS = [
+  'Africa',
+  'Americas',
+  'Asia',
+  'Europe',
+  'Oceania',
+  'Antarctic',
+] as const;
+
+export type Region = typeof REGIONS[number];
+
+export function isRegion(value: string): value is Region {
+  return REGIONS.includes(value as Region);
+}
--- src/app/country/pages/by-region-page/by-region-page.ts
+++ src/app/country/pages/by-region-page/by-region-page.ts
@@ -1,10 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
-import { CountrySearchInput } from "../../components/country-search-input/country-search-input";
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { CountryList } from "../../components/country-list/country-list";
-import { Region } from '../../interfaces/region.type';
+import { isRegion, Region, REGIONS } from '../../interfaces/region.type';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
 import { CountryService } from '../../services/country.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-region-page',
@@ -13,22 +13,32 @@ import { CountryService } from '../../services/country.service';
 })
 export class ByRegionPage {
 
-    public regions: Region[] = [
-    'Africa',
-    'Americas',
-    'Asia',
-    'Europe',
-    'Oceania',
-    'Antarctic',
-  ];
+  public regions = REGIONS
 
-  public selectedRegion = signal<Region | null>(null)
+  private activatedRoute = inject(ActivatedRoute)
+
+  private router = inject(Router)
+
+  private queryParam = this.activatedRoute.snapshot.queryParamMap.get('query')
+
+  public selectedRegion = linkedSignal<Region | null>(() => {
+    return this.queryParam && isRegion(this.queryParam)
+      ? this.queryParam
+      : null
+    }
+  )
+
   private countryService = inject(CountryService)
 
   countryResource = rxResource({
     params: () => ({ query: this.selectedRegion() }),
     stream: ({ params }) => {
-      if ( !params.query ) return of([])
+      if (!params.query) return of([])
+      this.router.navigate(['/country/by-region'], {
+        queryParams: {
+          query: params.query
+        }
+      })
       return this.countryService.searchByRegion(params.query)
     }
   })
